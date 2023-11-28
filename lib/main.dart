d18c98ef7b921e0f73c0de3ea2ae9462f3082925
Manuel <manuelfonseca114@gmail.com>
2023-11-28 17:57:21 -0600

    a

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Lab1',
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            title: const Text('Prueba de Flutter'),
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          body: const Center(
+            child: Image(
+              image: NetworkImage('https://www.utsalamanca.edu.mx/assets/img/pagina-principal/logouts.png'),
+            ),
+          )
+        ));
+  }
+}
